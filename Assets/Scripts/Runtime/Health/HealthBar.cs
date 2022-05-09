f250e02d87c28ff95c7803f6abe1fd9045ad212e
DomantasGarsvas <dgarsvas@gmail.com>
2022-05-09 16:14:20 +0300

    added healthbar enabling/disabling

--- Assets/Scripts/Runtime/Health/HealthBar.cs
+++ Assets/Scripts/Runtime/Health/HealthBar.cs
@@ -6,6 +6,9 @@ public class HealthBar : MonoBehaviour
     [SerializeField]
     private Slider slider;
     private HealthController healthRef;
+    private bool isShown;
+    private float showTimer;
+    private const float SHOW_DURATION = 2f;
 
     public void Initialize(HealthController health)
     {
@@ -17,7 +20,16 @@ public class HealthBar : MonoBehaviour
 
     private void Update()
     {
-        transform.position = Camera.main.WorldToScreenPoint(healthRef.transform.position);
+        if (showTimer <= 0f)
+        {
+            isShown = false;
+            gameObject.SetActive(false);
+        }
+        if (isShown)
+        {
+            transform.position = Camera.main.WorldToScreenPoint(healthRef.transform.position);
+            showTimer -= Time.deltaTime;
+        }
     }
 
     private void OnDestroy()
@@ -27,6 +39,9 @@ public class HealthBar : MonoBehaviour
 
     private void DamageTaken(float damageAmount, Vector3 dir)
     {
+        gameObject.SetActive(true);
+        isShown = true;
+        showTimer = SHOW_DURATION;
         slider.value = healthRef.Health;
     }
 }
